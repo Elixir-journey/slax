c4e5f86e598ace9663313eb340ded80b0b2aa797
Kevin Avignon <6740474+Kavignon@users.noreply.github.com>
2024-11-25 20:13:02 -0500

    Added some notes on Ecto schemas and Elixir type system.

--- lib/slax/chat/room.ex
+++ lib/slax/chat/room.ex
@@ -2,7 +2,14 @@ defmodule Slax.Chat.Room do
   use Ecto.Schema
   import Ecto.Changeset
 
+  # By default, a schema will automatically generate a primary key which is named id and of type :integer.
+  # The field macro defines a field in the schema with given name and type.
+  # Source: https://hexdocs.pm/ecto/Ecto.Schema.html
   schema "rooms" do
+    # Albeit DB systems might make a distinction for varchar/text/etc, the atom :string is the type that Elixir will use to
+    # manipulate the value associated with the column :field_name (e.g., :name). It's not referring to the Postgres value here.
+    # Once we have a recorded loaded from the database, it becomes a full-fledged string in the eyes of the Elixir type system.
+
     field :name, :string
     field :topic, :string
 
