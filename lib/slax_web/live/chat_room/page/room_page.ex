9b8429ec7d07534cde629bc3f4e6da4912e84b40
Kevin Avignon <6740474+Kavignon@users.noreply.github.com>
2025-02-08 10:25:07 -0500

    Toggle topic name on click.

--- lib/slax_web/live/chat_room/page/room_page.ex
+++ lib/slax_web/live/chat_room/page/room_page.ex
@@ -25,10 +25,19 @@ defmodule SlaxWeb.Live.ChatRoom.Page do
     room = ChatRoom.fetch_one_room()
 
     if is_nil(room) do
-      assign(socket, :slax_room, @default_chat_room)
+      socket
+      |> assign(slax_room: @default_chat_room)
+      |> assign(hide_topic?: false)
     else
-      assign(socket, :slax_room, room)
+      socket
+      |> assign(slax_room: room)
+      |> assign(hide_topic?: false)
     end
+  end
+
+  def handle_event("toggle-topic", _params, socket) do
+    {:noreply, socket}
+    {:noreply, assign(socket, hide_topic?: !socket.assigns.hide_topic?)}
   end
 
   def render(assigns) do
@@ -37,10 +46,18 @@ defmodule SlaxWeb.Live.ChatRoom.Page do
       <div class="flex justify-between items-center flex-shrink-0 h-16 bg-white border-b border-slate-300 px-4">
         <div class="flex flex-col gap-1.5">
           <h1 class="text-sm font-bold leading-none">
-            #room-name {@slax_room.name}
+            #room-name {@slax_room.name} yo
           </h1>
-          <div class="text-xs leading-none h-3.5">Placeholder topic</div>
-          <div class="text-xs leading-none h-3.5">{@slax_room.topic}</div>
+          <div
+            class={["text-xs leading-none h-3.5", @hide_topic? && "text-slate-600"]}
+            phx-click="toggle-topic"
+          >
+            <%= if @hide_topic? do %>
+              [Topic hidden]
+            <% else %>
+              {@slax_room.topic}
+            <% end %>
+          </div>
         </div>
       </div>
     </div>
